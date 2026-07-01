7faa83c608a93184c2d626f239958cd0a1cf0838
Sebastian <93546847+harry05555@users.noreply.github.com>
2026-07-01 18:21:10 +0200

    Überspringen Button eingeführt

--- src/main/java/de/gruppe_D/features/fragelebenslauf/FrageLebenslaufController.java
+++ src/main/java/de/gruppe_D/features/fragelebenslauf/FrageLebenslaufController.java
@@ -18,11 +18,19 @@ public class FrageLebenslaufController {
 
     private void init() {
         view.btnSave.addActionListener(benutzerinformationen());
+        view.btnSkip.addActionListener(skip());
     }
 
     private ActionListener benutzerinformationen() {
         return e -> {
             frageLebenslaufService.benutzerinformationen(view.tfVorname.getText(), view.tfName.getText(), view.tfAdresse.getText());
+//            todo Hier kommt noch die Route, es vollständig ausfüllen will
+        };
+    }
+
+    private ActionListener skip() {
+        return e -> {
+//           todo Hier kommt noch die Route, wenn man überspringen will
         };
     }
 }
